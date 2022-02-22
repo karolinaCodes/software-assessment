07f0aa80dcdc502e40a23c34d27d26277218cc5e
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-22 12:10:18 -0500

    add project set up

---
+++ main.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+const csv = require('csv-parser');
+const fs = require('fs');
+
+console.log(args);
+
+fs.createReadStream(args[0])
+  .pipe(csv())
+  .on('data', row => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
