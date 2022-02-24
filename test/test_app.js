016e571690b118a4738e720411db5340685f7423
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-24 13:11:41 -0500

    add first test file

---
+++ test/test_app.js
@@ -0,0 +1,14 @@
+const chai = require('chai'); // 1
+const assert = chai.assert;
+
+const shouldBuyCar = require('../shouldBuyCar.js'); // 2
+
+describe('#shouldBuyCar()', function () {
+  // 3
+
+  it("should return false if it's a hatchback", function () {
+    // 4
+  });
+});
+
+// create files for function or for all functions
