b6d4905f35b772f7fe0844b53e6f4185b4f64d62
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-02-24 12:17:45 -0500

    refactor helpers functions into helpers.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {emptyObject, deepClone, courseAvgForStudent} = require('./helpers');
 const csv = require('csv-parser');
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
@@ -19,25 +20,37 @@ new Promise((resolve, reject) => {
   // if error, reject
   fs.createReadStream(coursesFile)
     .pipe(csv())
-    .on('data', row => courses.push(row))
+    .on('data', row => {
+      if (emptyObject(row)) return;
+      courses.push(row);
+    })
     .on('end', () => {
       console.log(`"${coursesFile}" file parsed.`);
 
       fs.createReadStream(studentsFile)
         .pipe(csv())
-        .on('data', row => students.push(row))
+        .on('data', row => {
+          if (emptyObject(row)) return;
+          students.push(row);
+        })
         .on('end', () => {
           console.log(`"${studentsFile}" file parsed.`);
 
           fs.createReadStream(testsFile)
             .pipe(csv())
-            .on('data', row => tests.push(row))
+            .on('data', row => {
+              if (emptyObject(row)) return;
+              tests.push(row);
+            })
             .on('end', () => {
               console.log(`"${testsFile}" file parsed.`);
 
               fs.createReadStream(marksFile)
                 .pipe(csv())
-                .on('data', row => marks.push(row))
+                .on('data', row => {
+                  if (emptyObject(row)) return;
+                  marks.push(row);
+                })
                 .on('end', () => {
                   console.log(`"${marksFile}" file parsed.`);
                   resolve();
@@ -46,11 +59,12 @@ new Promise((resolve, reject) => {
         });
     });
 }).then(() => {
-  // create deep clone array of objects- so don't have unwanted bugs when use array methods to create new array from array of objects
-  const deepClone = array => JSON.parse(JSON.stringify(array));
+  console.log('students', students);
+  console.log('tests', tests);
+  console.log('courses', courses);
+  console.log('marks', marks);
 
   // for each student: create an object with student info and an array of courses and then each course average and finally totalAverage of all courses
-
   students.forEach(student => {
     // all tests a student did
     const testsIdsForStudent = deepClone(marks)
@@ -75,34 +89,16 @@ new Promise((resolve, reject) => {
       courseIdsforStudent.includes(course.id)
     );
 
-    // course average
-    const courseAvgForStudent = (studentId, courseId) => {
-      const marksForStudent = deepClone(marks).filter(
-        markObj => markObj.student_id === studentId
-      );
-
-      // all tests for the specific course that passed into ftn
-      const testsForCourse = deepClone(tests).filter(
-        test => test.course_id === courseId
-      );
-
-      const studentsMarksForCourse = marksForStudent.filter(mark =>
-        testsForCourse.find(test => test.id === mark.test_id)
-      );
-
-      const courseAvg = studentsMarksForCourse.reduce((acc, curr) => {
-        // find the test that goes with the students mark
-        const testItem = testsForCourse.find(test => test.id === curr.test_id);
-        return acc + curr.mark * (testItem.weight / 100);
-      }, 0);
-      return courseAvg.toFixed(1);
-    };
-
     // a student is considered to be enrolled in a course if they have taken a least one test for that course
     student.courses = [];
     // TODO: change order of courses and totalAverage
     coursesForStudent.forEach(course => {
-      course.courseAverage = courseAvgForStudent(student.id, course.id);
+      course.courseAverage = courseAvgForStudent(
+        student.id,
+        course.id,
+        marks,
+        tests
+      );
       student.courses.push(course);
     });
 
---
+++ helpers.js
@@ -0,0 +1,30 @@
+// if the object is empty because the csv parser parsed empty space, don't add to array
+const emptyObject = obj => Object.keys(obj).length === 0;
+
+// create deep clone array of objects- so don't have unwanted bugs when use array methods to create new array from array of objects
+const deepClone = array => JSON.parse(JSON.stringify(array));
+
+// course average
+const courseAvgForStudent = (studentId, courseId, marks, tests) => {
+  const marksForStudent = deepClone(marks).filter(
+    markObj => markObj.student_id === studentId
+  );
+
+  // all tests for the specific course that passed into ftn
+  const testsForCourse = deepClone(tests).filter(
+    test => test.course_id === courseId
+  );
+
+  const studentsMarksForCourse = marksForStudent.filter(mark =>
+    testsForCourse.find(test => test.id === mark.test_id)
+  );
+
+  const courseAvg = studentsMarksForCourse.reduce((acc, curr) => {
+    // find the test that goes with the students mark
+    const testItem = testsForCourse.find(test => test.id === curr.test_id);
+    return acc + curr.mark * (testItem.weight / 100);
+  }, 0);
+  return courseAvg.toFixed(1);
+};
+
+module.exports = {emptyObject, deepClone, courseAvgForStudent};
